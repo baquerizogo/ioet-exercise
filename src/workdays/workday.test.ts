4233b73ed2d9e1515680416feb21837534f008fb
Jabgomez <josebaquerizo_1999@hotmail.com>
2022-12-03 16:06:25 -0500

    Add workdays tests

--- src/workdays/workday.test.ts
+++ src/workdays/workday.test.ts
@@ -1,4 +1,4 @@
-import { areHoursValid } from "./workday.service";
+import { areHoursValid, isDay, parseEmployeeWorkdays } from "./workday.service";
 
 test('Return false if entry and departure hour are inverted', () => {
     expect(areHoursValid(1800, 1400)).toBeFalsy();
@@ -6,4 +6,21 @@ test('Return false if entry and departure hour are inverted', () => {
 
 test('Return false if entry or departure hour are not time values', () => {
     expect(areHoursValid(2700, 3400)).toBeFalsy();
+})
+
+test('Return true if input day is type Days', () => {
+    expect(() => isDay('MO')).toBeTruthy();
+})
+
+test('Return false if input day is not type Days', () => {
+    expect(() => isDay('ME')).toBeTruthy();
+})
+
+test('Throw error if input has invalid format', () => {
+    expect(() => parseEmployeeWorkdays('MO10:0012:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00')).toThrowError();
+})
+
+test('Return an array with parsed workdays', () => {
+    const workdays = parseEmployeeWorkdays('MO10:00-12:00,SU20:00-21:00');
+    expect(workdays.length > 0).toBeTruthy();
 })
